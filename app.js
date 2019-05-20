20a9b4dbe6a3e8e032b53539f93c4e3db9db7e85
Prashant-Acharya <pacharya97.pa@gmail.com>
2019-05-20 18:53:44 +0545

    routes to get and add movie defined

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
+
+// Import Routes
+const movieRoutes = require('./routes/getmovies')
+app.use('/allmovies', movieRoutes)
+
+// Add Routes
+const addMovie = require('./routes/add')
+app.use('/add', addMovie)
+
+
+// Connect to a DB
+mongoose.connect('mongodb+srv://prashant:default@cluster0-sk79g.mongodb.net/test?retryWrites=true',
+	{ useNewUrlParser: true },
+	() => {
+	console.log('Connected to a DB.')
+})
+
+
+app.listen(5000)
+
+
